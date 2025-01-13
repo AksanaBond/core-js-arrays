23bf76314095de365f2a0ee8d4df32a41b0e4661
AksanaBond <oksana@MacBook-Air-Oksana.local>
2025-01-13 15:35:47 +0100

    feat: pass task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -484,8 +484,12 @@ function getHexRGBValues(/* arr */) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  if (arr.length === 0) {
+    return [];
+  }
+  const sortArray = arr.sort((a, b) => b - a);
+  return sortArray.slice(0, n);
 }
 
 /**
@@ -500,8 +504,8 @@ function getMaxItems(/* arr, n */) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  return arr1.filter((element) => arr2.includes(element));
 }
 
 /**
@@ -550,8 +554,14 @@ function propagateItemsByPositionIndex(/* arr */) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const position = n;
+  if (arr.length === position) {
+    return arr;
+  }
+  return arr
+    .slice(Math.abs(position) + 1)
+    .concat(arr.slice(0, Math.abs(position) + 1));
 }
 
 /**
